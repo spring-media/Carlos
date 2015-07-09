6e46691884b95cf80fc558b43246dfb07f01193a
Vittorio Monaco <vittorio.monaco@weltn24.de>
2015-07-09 17:10:50 +0200

    Adds a nice operator for the conditioned func

--- Carlos/Carlos.swift
+++ Carlos/Carlos.swift
@@ -148,6 +148,22 @@ public class CacheRequest<T> {
   }
 }
 
+infix operator <?> { associativity right }
+
+/**
+Wraps a CacheLevel with a boolean condition on the key that controls when a get call should fail unconditionally
+
+:param: cache The cache level you want to decorate
+:param: condition The condition closure that takes a key and returns true whether the key could be fetched, or false whether the get should fail unconditionally. The closure also returns an optional error in case it wants to explicitly communicate why it failed. In case no error is returned, a default FetchError.ConditionNotSatisfied is used instead.
+
+:returns: A new BasicCache that will check for the condition before every get is dispatched to the decorated cache level
+
+:discussion: The condition doesn't apply to the set, clear, onMemoryWarning calls
+*/
+public func <?><A: CacheLevel, B where A.KeyType == B>(condition: (B) -> (Bool, NSError?), cache: A) -> BasicCache<B, A.OutputType> {
+  return conditioned(cache, condition)
+}
+
 /**
 Wraps a CacheLevel with a boolean condition on the key that controls when a get call should fail unconditionally
 
