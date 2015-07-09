48883651a59a05b6863dab6d7ff9f4e13500b5a2
Vittorio Monaco <vittorio.monaco@weltn24.de>
2015-07-09 17:09:39 +0200

    Condition in the conditioned func now also returns an optional NSError

--- Carlos/Carlos.swift
+++ Carlos/Carlos.swift
@@ -152,23 +152,24 @@ public class CacheRequest<T> {
 Wraps a CacheLevel with a boolean condition on the key that controls when a get call should fail unconditionally
 
 :param: cache The cache level you want to decorate
-:param: condition The condition closure that takes a key and returns true whether the key could be fetched, or false whether the get should fail unconditionally.
+:param: condition The condition closure that takes a key and returns true whether the key could be fetched, or false whether the get should fail unconditionally. The closure also returns an optional error in case it wants to explicitly communicate why it failed. In case no error is returned, a default FetchError.ConditionNotSatisfied is used instead.
 
 :returns: A new BasicCache that will check for the condition before every get is dispatched to the decorated cache level
 
 :discussion: The condition doesn't apply to the set, clear, onMemoryWarning calls
 */
-public func conditioned<A: CacheLevel, B where A.KeyType == B>(cache: A, condition: (B) -> Bool) -> BasicCache<B, A.OutputType> {
+public func conditioned<A: CacheLevel, B where A.KeyType == B>(cache: A, condition: (B) -> (Bool, NSError?)) -> BasicCache<B, A.OutputType> {
   return BasicCache<B, A.OutputType>(
     getClosure: { (key) in
       let request = CacheRequest<A.OutputType>()
       
-      if condition(key) {
+      let (passesCondition, error) = condition(key)
+      if passesCondition {
         cache.get(key).onSuccess({ result in
           request.succeed(result)
         })
       } else {
-        request.fail(errorWithCode(FetchError.ConditionNotSatisfied.rawValue))
+        request.fail(error ?? errorWithCode(FetchError.ConditionNotSatisfied.rawValue))
       }
       
       return request
