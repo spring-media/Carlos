c7c1eef102f25574a9f4ace44351bf9991580657
Vittorio Monaco <vittorio.monaco1@gmail.com>
2015-07-10 07:22:45 +0200

    Cocoa pods support with submodule approach

--- Sample/Pods/Target Support Files/Pods-CarlosTests/Pods-CarlosTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CarlosTests : NSObject
-@end
-@implementation PodsDummy_Pods_CarlosTests
-@end
--- Sample/Pods/Target Support Files/Pods-CarlosTests/Pods-CarlosTests-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_CarlosTestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_CarlosTestsVersionString[];
-
