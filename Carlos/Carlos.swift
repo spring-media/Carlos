1ad0f70971d001614acfb9906d89fa56840d9d56
Vittorio Monaco <vittorio.monaco1@gmail.com>
2015-07-11 20:46:49 +0200

    Moves CacheLevel back to Carlos.swift

--- Carlos/Carlos.swift
+++ Carlos/Carlos.swift
@@ -14,3 +14,39 @@ internal func wrapClosureIntoCacheLevel<A, B>(closure: (key: A) -> CacheRequest<
 internal func wrapClosureIntoOneWayTransformer<A, B>(transformerClosure: A -> B) -> OneWayTransformationBox<A, B> {
   return OneWayTransformationBox<A, B>(transform: transformerClosure)
 }
+
+/// An abstraction for a generic cache level
+public protocol CacheLevel {
+  /// A typealias for the key the cache level accepts
+  typealias KeyType
+  
+  /// A typealias for the data the cache returns in the success closure
+  typealias OutputType
+  
+  /**
+  Tries to get a value from the cache level
+  
+  :param: fetchable The key of the value you would like to get
+  
+  :returns: a CacheRequest that you can attach success and failure closures to
+  */
+  func get(fetchable: KeyType) -> CacheRequest<OutputType>
+  
+  /**
+  Tries to set a value on the cache level
+  
+  :param: value The bytes to set on the cache level
+  :param: fetchable The key of the value you're trying to set
+  */
+  func set(value: OutputType, forKey fetchable: KeyType)
+  
+  /**
+  Asks to clear the cache level
+  */
+  func clear()
+  
+  /**
+  Notifies the cache level that a memory warning was thrown, and asks it to do its best to clean some memory
+  */
+  func onMemoryWarning()
+}
--- Carlos/Pooled.swift
+++ Carlos/Pooled.swift
@@ -10,39 +10,3 @@ Wraps a CacheLevel with a requests pool
 public func pooled<A: CacheLevel where A.KeyType: Hashable>(cache: A) -> PoolCache<A.KeyType, A.OutputType, A> {
   return PoolCache<A.KeyType, A.OutputType, A>(internalCache: cache)
 }
-
-/// An abstraction for a generic cache level
-public protocol CacheLevel {
-  /// A typealias for the key the cache level accepts
-  typealias KeyType
-  
-  /// A typealias for the data the cache returns in the success closure
-  typealias OutputType
-  
-  /**
-  Tries to get a value from the cache level
-  
-  :param: fetchable The key of the value you would like to get
-  
-  :returns: a CacheRequest that you can attach success and failure closures to
-  */
-  func get(fetchable: KeyType) -> CacheRequest<OutputType>
-  
-  /**
-  Tries to set a value on the cache level
-  
-  :param: value The bytes to set on the cache level
-  :param: fetchable The key of the value you're trying to set
-  */
-  func set(value: OutputType, forKey fetchable: KeyType)
-  
-  /**
-  Asks to clear the cache level
-  */
-  func clear()
-  
-  /**
-  Notifies the cache level that a memory warning was thrown, and asks it to do its best to clean some memory
-  */
-  func onMemoryWarning()
-}
