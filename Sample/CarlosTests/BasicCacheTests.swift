1cb51f64f96087e5bb390b5c1869864ca6a82915
Vittorio Monaco <vittorio.monaco1@gmail.com>
2015-07-12 13:37:42 +0200

    Adds tests for BasicCache

---
+++ Sample/CarlosTests/BasicCacheTests.swift
@@ -0,0 +1,111 @@
+import Foundation
+import Quick
+import Nimble
+import Carlos
+
+class BasicCacheTests: QuickSpec {
+  override func spec() {
+    describe("BasicCache") {
+      var cache: BasicCache<String, Int>!
+      var numberOfTimesCalledClear = 0
+      var numberOfTimesCalledOnMemoryWarning = 0
+      var numberOfTimesCalledGet = 0
+      var numberOfTimesCalledSet = 0
+      var didSetKey: String?
+      var didSetValue: Int?
+      var didGetKey: String?
+      var fakeRequest: CacheRequest<Int>!
+      
+      beforeEach {
+        numberOfTimesCalledClear = 0
+        numberOfTimesCalledGet = 0
+        numberOfTimesCalledOnMemoryWarning = 0
+        numberOfTimesCalledSet = 0
+        
+        fakeRequest = CacheRequest<Int>()
+        
+        cache = BasicCache<String, Int>(
+          getClosure: { key in
+            didGetKey = key
+            numberOfTimesCalledGet++
+            
+            return fakeRequest
+          },
+          setClosure: { (key, value) in
+            didSetKey = key
+            didSetValue = value
+            numberOfTimesCalledSet++
+          },
+          clearClosure: {
+            numberOfTimesCalledClear++
+          },
+          memoryClosure: {
+            numberOfTimesCalledOnMemoryWarning++
+          }
+        )
+      }
+      
+      context("when calling get") {
+        let key = "key to test"
+        var request: CacheRequest<Int>!
+        
+        beforeEach {
+          request = cache.get(key)
+        }
+        
+        it("should call the closure") {
+          expect(numberOfTimesCalledGet).to(equal(1))
+        }
+        
+        it("should pass the right key") {
+          expect(didGetKey).to(equal(key))
+        }
+        
+        it("should not modify the request") {
+          expect(request).to(beIdenticalTo(fakeRequest))
+        }
+      }
+      
+      context("when calling set") {
+        let key = "test key"
+        let value = 101
+        
+        beforeEach {
+          cache.set(value, forKey: key)
+        }
+        
+        it("should call the closure") {
+          expect(numberOfTimesCalledSet).to(equal(1))
+        }
+        
+        it("should pass the right key") {
+          expect(didSetKey).to(equal(key))
+        }
+        
+        it("should pass the right value") {
+          expect(didSetValue).to(equal(value))
+        }
+      }
+      
+      context("when calling clear") {
+        beforeEach {
+          cache.clear()
+        }
+        
+        it("should call the closure") {
+          expect(numberOfTimesCalledClear).to(equal(1))
+        }
+      }
+      
+      context("when calling onMemoryWarning") {
+        beforeEach {
+          cache.onMemoryWarning()
+        }
+        
+        it("should call the closure") {
+          expect(numberOfTimesCalledOnMemoryWarning).to(equal(1))
+        }
+      }
+    }
+  }
+}
