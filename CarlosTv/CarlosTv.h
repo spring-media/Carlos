3ad289f3d6ee5a066306f302f2fffd549c6383f3
Vittorio Monaco <vittorio.monaco1@gmail.com>
2015-10-03 19:22:18 +0200

    Initial support for tvOS for #59

---
+++ CarlosTv/CarlosTv.h
@@ -0,0 +1,19 @@
+//
+//  CarlosTv.h
+//  CarlosTv
+//
+//  Created by Vittorio Monaco on 03/10/15.
+//  Copyright © 2015 WeltN24. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CarlosTv.
+FOUNDATION_EXPORT double CarlosTvVersionNumber;
+
+//! Project version string for CarlosTv.
+FOUNDATION_EXPORT const unsigned char CarlosTvVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CarlosTv/PublicHeader.h>
+
+#import <CarlosTv/NSKeyedUnarchiver+SwiftUtilities.h>
