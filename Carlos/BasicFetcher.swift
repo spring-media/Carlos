224b3c3ac2a20e4b7746fad459ab4a4fda96ef94
Vittorio Monaco <vittorio.monaco1@gmail.com>
2015-10-03 21:35:07 +0200

    Adds documentation, improves the type information for the fetcher transformation, removes redundant TwoWayTransformer transformations applied to fetch closures since they can be best treated as OneWayTransformer transformations applied to fetchers.

--- Carlos/BasicFetcher.swift
+++ Carlos/BasicFetcher.swift
@@ -1,15 +1,28 @@
 import Foundation
 
+/// A wrapper fetcher that explicitly takes a get closure
 public final class BasicFetcher<A, B>: Fetcher {
   public typealias KeyType = A
   public typealias OutputType = B
   
   private let getClosure: (key: A) -> CacheRequest<B>
   
+  /**
+   Initializes a new instance of a BasicFetcher specifying a get closure, thus determining the behavior of the fetcher as a whole
+   
+   - parameter getClosure: The closure to execute when you call get(key) on this instance
+   */
   public init(getClosure: (key: A) -> CacheRequest<B>) {
     self.getClosure = getClosure
   }
   
+  /**
+   Asks the fetcher to get the value for a given key
+   
+   - parameter key: The key you want to get the value for
+   
+   - returns: The result of the getClosure specified when initializing the instance
+   */
   public func get(key: KeyType) -> CacheRequest<OutputType> {
     return getClosure(key: key)
   }
--- Carlos/FetcherValueTransformation.swift
+++ Carlos/FetcherValueTransformation.swift
@@ -1,27 +1,77 @@
 import Foundation
 
 extension Fetcher {
+  
+  /**
+   Applies a transformation to the fetcher
+   The transformation works by changing the type of the value the fetcher returns when succeeding
+   Use this transformation when you store a value type but want to mount the fetcher in a pipeline that works with other value types
+   
+   - parameter transformer: The transformation you want to apply
+   
+   - returns: A new fetcher result of the transformation of the original fetcher
+   */
   public func transformValues<A: OneWayTransformer where OutputType == A.TypeIn>(transformer: A) -> BasicFetcher<KeyType, A.TypeOut> {
     return self =>> transformer
   }
 }
 
+/**
+ Applies a transformation to a fetch closure
+ The transformation works by changing the type of the value the fetcher returns when succeeding
+ Use this transformation when you store a value type but want to mount the fetcher in a pipeline that works with other value types
+ 
+ - parameter fetchClosure: The fetcher closure you want to transform
+ - parameter transformer: The transformation you want to apply
+ 
+ - returns: A new fetcher result of the transformation of the original fetcher
+ */
 public func transformValues<A, B: OneWayTransformer>(fetchClosure: (key: A) -> CacheRequest<B.TypeIn>, transformer: B) -> BasicFetcher<A, B.TypeOut> {
   return transformValues(wrapClosureIntoFetcher(fetchClosure), transformer: transformer)
 }
 
-public func transformValues<A: CacheLevel, B: OneWayTransformer where A.OutputType == B.TypeIn>(cache: A, transformer: B) -> BasicFetcher<A.KeyType, B.TypeOut> {
+/**
+ Applies a transformation to a fetcher
+ The transformation works by changing the type of the value the fetcher returns when succeeding
+ Use this transformation when you store a value type but want to mount the fetcher in a pipeline that works with other value types
+ 
+ - parameter fetcher: The fetcher you want to transform
+ - parameter transformer: The transformation you want to apply
+ 
+ - returns: A new fetcher result of the transformation of the original fetcher
+ */
+public func transformValues<A: Fetcher, B: OneWayTransformer where A.OutputType == B.TypeIn>(fetcher: A, transformer: B) -> BasicFetcher<A.KeyType, B.TypeOut> {
   return BasicFetcher(
     getClosure: { key in
-      return cache.get(key).mutate(transformer)
+      return fetcher.get(key).mutate(transformer)
     }
   )
 }
 
+/**
+ Applies a transformation to a fetch closure
+ The transformation works by changing the type of the value the fetcher returns when succeeding
+ Use this transformation when you store a value type but want to mount the fetcher in a pipeline that works with other value types
+ 
+ - parameter fetchClosure: The fetch closure you want to transform
+ - parameter transformer: The transformation you want to apply
+ 
+ - returns: A new fetcher result of the transformation of the original fetcher
+ */
 public func =>><A, B: OneWayTransformer>(fetchClosure: (key: A) -> CacheRequest<B.TypeIn>, transformer: B) -> BasicFetcher<A, B.TypeOut> {
   return transformValues(wrapClosureIntoFetcher(fetchClosure), transformer: transformer)
 }
 
-public func =>><A: CacheLevel, B: OneWayTransformer where A.OutputType == B.TypeIn>(cache: A, transformer: B) -> BasicFetcher<A.KeyType, B.TypeOut> {
-  return transformValues(cache, transformer: transformer)
+/**
+ Applies a transformation to a fetcher
+ The transformation works by changing the type of the value the fetcher returns when succeeding
+ Use this transformation when you store a value type but want to mount the fetcher in a pipeline that works with other value types
+ 
+ - parameter fetcher: The fetcher you want to transform
+ - parameter transformer: The transformation you want to apply
+ 
+ - returns: A new fetcher result of the transformation of the original fetcher
+ */
+public func =>><A: Fetcher, B: OneWayTransformer where A.OutputType == B.TypeIn>(fetcher: A, transformer: B) -> BasicFetcher<A.KeyType, B.TypeOut> {
+  return transformValues(fetcher, transformer: transformer)
 }
