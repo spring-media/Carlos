8323b4815b8ca3cf4414a18c07b1c4a4d09744cb
Vittorio Monaco <vittorio.monaco1@gmail.com>
2016-01-24 16:09:44 +0100

    Adds a batchGetSome API if the batch request should not fail when one or more get requests fail or are canceled

--- Carlos/CacheLevel+Batch.swift
+++ Carlos/CacheLevel+Batch.swift
@@ -8,7 +8,7 @@ extension CacheLevel {
    
    - returns: A Future that will call the success callback when ALL the keys will be fetched successfully, and the failure callback as soon as JUST ONE of the keys cannot be fetched.
    */
-  public func batchGet(keys: [KeyType]) -> Future<[OutputType]> {
+  public func batchGetAll(keys: [KeyType]) -> Future<[OutputType]> {
     let result = Promise<[OutputType]>()
     let lock: ReadWriteLock = PThreadReadWriteLock()
     var intermediateResults = Array<OutputType?>(count: keys.count, repeatedValue: nil)
@@ -27,6 +27,42 @@ extension CacheLevel {
         }
         .onFailure(result.fail)
         .onCancel(result.cancel)
+    }
+    
+    return result.future
+  }
+  
+  /**
+   Performs a batch of get requests on this CacheLevel
+   
+   - parameter keys: The list of keys to batch
+   
+   - returns: A Future that will call the success callback when all the keys will be either fetched or failed, passing a list containing just the successful results
+   */
+  public func batchGetSome(keys: [KeyType]) -> Future<[OutputType]> {
+    let result = Promise<[OutputType]>()
+    let resultsLock: ReadWriteLock = PThreadReadWriteLock()
+    let counterLock: ReadWriteLock = PThreadReadWriteLock()
+    var completedRequests = 0
+    var intermediateResults = Array<OutputType?>(count: keys.count, repeatedValue: nil)
+    
+    keys.enumerate().forEach { (iteration, key) in
+      get(key)
+        .onCompletion { value, _ in
+          if let value = value {
+            resultsLock.withWriteLock {
+              intermediateResults[iteration] = value
+            }
+          }
+          
+          counterLock.withWriteLock {
+            completedRequests += 1
+            
+            if completedRequests == keys.count {
+              result.succeed(intermediateResults.flatMap { $0 })
+            }
+          }
+        }
     }
     
     return result.future
