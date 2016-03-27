c8f8215cfa78ca468fe0813cb80e477781b4db99
Vittorio Monaco <vittorio.monaco1@gmail.com>
2016-03-27 12:43:33 +0200

    Implements Future.map

---
+++ Futures/Future+Map.swift
@@ -0,0 +1,111 @@
+enum FutureMappingError: ErrorType {
+  case CantMapValue
+}
+
+extension Future {
+  private func _map<U>(handler: (T, Promise<U>) -> Void) -> Future<U> {
+    let mapped = Promise<U>()
+    
+    self.onCompletion { result in
+      switch result {
+      case .Success(let value):
+        handler(value, mapped)
+      case .Error(let error):
+        mapped.fail(error)
+      case .Cancelled:
+        mapped.cancel()
+      }
+    }
+    
+    return mapped.future
+  }
+  
+  /**
+  Maps a Future<T> into a Future<U> through a function that takes a value of type T and returns a value of type U
+   
+  - parameter f: The closure that takes a value of type T and returns a value of type U
+  
+  - returns: A new Future<U> that will behave as the original one w.r.t. cancelation and failure, but will succeed with a value of type U obtained through the given closure
+  */
+  public func map<U>(f: T -> U) -> Future<U> {
+    return _map { value, mapped in
+      mapped.succeed(f(value))
+    }
+  }
+  
+  /**
+   Maps a Future<T> into a Future<U> through a function that takes a value of type T and returns a value of type U
+   
+   - parameter f: The closure that takes a value of type T and returns a value of type U. Please note the closure can throw
+   
+   - returns: A new Future<U> that will behave as the original one w.r.t. cancelation and failure, but will succeed with a value of type U obtained through the given closure, unless the latter throws. In this case, the new Future will fail
+   */
+  public func map<U>(f: T throws -> U) -> Future<U> {
+    return _map { value, mapped in
+      do {
+        mapped.succeed(try f(value))
+      } catch {
+        mapped.fail(error)
+      }
+    }
+  }
+  
+  /**
+   Maps a Future<T> into a Future<U> through a function that takes a value of type T and returns a value of type U?
+   
+   - parameter f: The closure that takes a value of type T and returns a value of type U?
+   
+   - returns: A new Future<U> that will behave as the original one w.r.t. cancelation and failure, but will succeed with a value of type U obtained through the given closure, unless the latter returns nil. In this case, the new Future will fail
+   */
+  public func map<U>(f: T -> U?) -> Future<U> {
+    return _map { value, mapped in
+      if let mappedValue = f(value) {
+        mapped.succeed(mappedValue)
+      } else {
+        mapped.fail(FutureMappingError.CantMapValue)
+      }
+    }
+  }
+  
+  /**
+   Maps a Future<T> into a Future<U> through a function that takes a value of type T and returns a Result<U>
+   
+   - parameter f: The closure that takes a value of type T and returns a Result<U>
+   
+   - returns: A new Future<U> that will behave as the original one w.r.t. cancelation and failure, but will succeed with a value of type U obtained through the given closure if the returned Result is a success. Otherwise, the new Future will fail or get canceled depending on the state of the returned Result
+   */
+  func map<U>(f: T -> Result<U>) -> Future<U> {
+    return _map { value, mapped in
+      switch f(value) {
+      case .Success(let value):
+        mapped.succeed(value)
+      case .Error(let error):
+        mapped.fail(error)
+      case .Cancelled:
+        mapped.cancel()
+      }
+    }
+  }
+  
+  /**
+   Maps a Future<T> into a Future<U> through a function that takes a value of type T and returns a Future<U>
+   
+   - parameter f: The closure that takes a value of type T and returns a Future<U>
+   
+   - returns: A new Future<U> that will behave as the original one w.r.t. cancelation and failure, but will succeed with a value of type U when the given Future will succeed. If the given Future fails or is canceled, the new Future will do so too.
+   */
+  public func map<U>(f: T -> Future<U>) -> Future<U> {
+    return _map { value, mapped in
+      f(value).onCompletion { result in
+        switch result {
+        case .Success(let value):
+          mapped.succeed(value)
+        case .Error(let error):
+          mapped.fail(error)
+        case .Cancelled:
+          mapped.cancel()
+        }
+      }
+    }
+  }
+}
---
+++ FuturesTests/Future+MapTests.swift
@@ -0,0 +1,111 @@
+import Quick
+import Nimble
+import PiedPiper
+
+class FutureMapTests: QuickSpec {
+  override func spec() {
+    describe("Mapping a Future") {
+      var promise: Promise<String>!
+      var mappedFuture: Future<Int>!
+      var successValue: Int?
+      var failureValue: ErrorType?
+      var wasCanceled: Bool!
+      
+      beforeEach {
+        promise = Promise<String>()
+        
+        wasCanceled = false
+        successValue = nil
+        failureValue = nil
+      }
+      
+      context("when done through a simple closure") {
+        let mappingClosure: String -> Int = { str in
+          return 1
+        }
+        
+        beforeEach {
+          mappedFuture = promise.future
+            .map(mappingClosure)
+            
+          mappedFuture.onCompletion { result in
+              switch result {
+              case .Success(let value):
+                successValue = value
+              case .Error(let error):
+                failureValue = error
+              case .Cancelled:
+                wasCanceled = true
+              }
+          }
+        }
+        
+        context("when the original future fails") {
+          let error = TestError.SimpleError
+          
+          beforeEach {
+            promise.fail(error)
+          }
+          
+          it("should also fail the mapped future") {
+            expect(failureValue).notTo(beNil())
+          }
+          
+          it("should fail the mapped future with the same error") {
+            expect(failureValue as? TestError).to(equal(error))
+          }
+          
+          it("should not succeed the mapped future") {
+            expect(successValue).to(beNil())
+          }
+          
+          it("should not cancel the mapped future") {
+            expect(wasCanceled).to(beFalse())
+          }
+        }
+        
+        context("when the original future is canceled") {
+          beforeEach {
+            promise.cancel()
+          }
+          
+          it("should also cancel the mapped future") {
+            expect(wasCanceled).to(beTrue())
+          }
+          
+          it("should not succeed the mapped future") {
+            expect(successValue).to(beNil())
+          }
+          
+          it("should not fail the mapped future") {
+            expect(failureValue).to(beNil())
+          }
+        }
+        
+        context("when the original future succeeds") {
+          let result = "Eureka!"
+          
+          beforeEach {
+            promise.succeed(result)
+          }
+          
+          it("should also succeed the mapped future") {
+            expect(successValue).notTo(beNil())
+          }
+          
+          it("should succeed the mapped future with the mapped value") {
+            expect(successValue).to(equal(mappingClosure(result)))
+          }
+          
+          it("should not fail the mapped future") {
+            expect(failureValue).to(beNil())
+          }
+          
+          it("should not cancel the mapped future") {
+            expect(wasCanceled).to(beFalse())
+          }
+        }
+      }
+    }
+  }
+}
