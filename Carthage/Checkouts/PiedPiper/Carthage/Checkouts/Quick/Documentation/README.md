a54e39ad81f03b2a9b0fafe4acd2d8134ef863b0
asalom <alex.salom@gmail.com>
2017-09-25 14:15:33 +0200

    remove carthage checkouts from repo

--- Carthage/Checkouts/PiedPiper/Carthage/Checkouts/Quick/Documentation/README.md
+++
@@ -1,5 +0,0 @@
-# Documentation
-
-- [English](en-us/README.md)
-- [日本語](ja/README.md)
-- [中文](zh-cn/README.md)
--- Carthage/Checkouts/PiedPiper/PiedPiper/Future+All.swift
+++
@@ -1,8 +0,0 @@
-extension Sequence where Iterator.Element: Async {
-  /**
-  - returns: A Future that succeeds when all the Futures contained in this sequence succeed, and fails when one of the Futures contained in this sequence fails.
-   */
-  public func all() -> Future<()> {
-    return mergeAll().map { _ in }
-  }
-}
