18f2cb396ac89b5c4764637ec48a9bf49da486f5
Artem Belenkov <artem.belenkov@spring-media.de>
2018-10-10 15:41:13 +0200

    changed md5 string method

--- Carlos/Carlos.h
+++ Carlos/Carlos.h
@@ -9,3 +9,4 @@ FOUNDATION_EXPORT const unsigned char CarlosVersionString[];
 // In this header, you should import all the public headers of your framework using statements like #import <Carlos/PublicHeader.h>
 
 #import <Carlos/NSKeyedUnarchiver+SwiftUtilities.h>
+#import <CommonCrypto/CommonCrypto.h>
