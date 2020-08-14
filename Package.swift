d0e79fe2eb1485041c77f5e50019921af4287986
ivanlisovyi <lisovyi.ivan@gmail.com>
2020-08-14 13:27:42 +0200

    Update PiedPiper to master version

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
       targets: ["Carlos"]),
   ],
   dependencies: [
-    .package(url: "https://github.com/spring-media/PiedPiper", .branch("feature/update-quick-and-nimble")),
+    .package(url: "https://github.com/spring-media/PiedPiper", .branch("master")),
     .package(url: "https://github.com/Quick/Quick.git", .upToNextMajor(from: "3.0.0")),
     .package(url: "https://github.com/Quick/Nimble.git", .upToNextMajor(from: "8.1.0")),
   ],
