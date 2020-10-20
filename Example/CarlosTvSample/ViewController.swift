adf8c104992c86308c6477e8d404a05f17f0613e
ivanlisovyi <lisovyi.ivan@gmail.com>
2020-10-20 18:16:25 +0200

    Fix CarlosTvSample building error

--- Example/CarlosTvSample/ViewController.swift
+++ Example/CarlosTvSample/ViewController.swift
@@ -1,6 +1,6 @@
 import UIKit
 import Carlos
-import PiedPiper
+import Combine
 
 class BitcoinResult {
   let USDValue: Float
@@ -21,34 +21,35 @@ enum SampleError: Error {
 }
 
 class ViewController: UIViewController {
-  
+  private var cancellables = Set<AnyCancellable>()
   
   override func viewDidLoad() {
     super.viewDidLoad()
     
-    let JSONFetcher: BasicFetcher<URL, AnyObject> =  NetworkFetcher().transformValues(JSONTransformer())
+    let JSONFetcher: BasicFetcher<URL, AnyObject> = NetworkFetcher().transformValues(JSONTransformer())
     let cache = JSONFetcher.transformValues(BTCTransformer())
     
-    cache.get(URL(string: "http://coinabul.com/api.php")!).onSuccess { result in
-      print("Bitcoin value is \(result.USDValue) USD")
-    }
+    cache.get(URL(string: "http://coinabul.com/api.php")!)
+      .sink(receiveCompletion: { completion in
+        print(completion)
+      }, receiveValue: { result in
+        print("Bitcoin value is \(result.USDValue) USD")
+      }).store(in: &cancellables)
   }
 }
 
 struct BTCTransformer: OneWayTransformer {
-  func transform(_ val: AnyObject) -> Future<BitcoinResult> {
-    let result = Promise<BitcoinResult>()
-    
-    if let JSON = val as? [String: AnyObject],
-      let BTCDict = JSON["BTC"] as? [String: AnyObject],
-      let USDStringValue = BTCDict["USD"] as? String,
-      let USDFloatValue = Float(USDStringValue)
-    {
-      result.succeed(BitcoinResult(USDValue: USDFloatValue))
-    } else {
-      result.fail(SampleError.invalidJSON)
-    }
-    
-    return result.future
+  func transform(_ val: AnyObject) -> AnyPublisher<BitcoinResult, Error> {
+    Future { promise in
+      if let JSON = val as? [String: AnyObject],
+        let BTCDict = JSON["BTC"] as? [String: AnyObject],
+        let USDStringValue = BTCDict["USD"] as? String,
+        let USDFloatValue = Float(USDStringValue)
+      {
+        promise(.success(BitcoinResult(USDValue: USDFloatValue)))
+      } else {
+        promise(.failure(SampleError.invalidJSON))
+      }
+    }.eraseToAnyPublisher()
   }
 }
