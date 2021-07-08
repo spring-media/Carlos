4107ed75d12c8db1d6201e5a21cb6e7ca464871d
ivanlisovyi <lisovyi.ivan@gmail.com>
2021-07-08 15:43:15 +0200

    Add code documentation for remove functionality

--- Sources/Carlos/Carlos.swift
+++ Sources/Carlos/Carlos.swift
@@ -26,9 +26,14 @@ public protocol CacheLevel: AnyObject {
   /// - Parameter value: The bytes to set on the cache level
   /// - Parameter key: The key of the value you're trying to set
   ///
-  /// - Returns: A `Publisher that will reflect the status of the set operation
+  /// - Returns: A `Publisher` that will reflect the status of the set operation
   func set(_ value: OutputType, forKey key: KeyType) -> AnyPublisher<Void, Error>
 
+  /// Remove value from cache for a given key.
+  ///
+  /// - Parameter key: They key of the value to be removed
+  ///
+  /// - Returns: A `Publisher` that reflects a status of the remove operation.
   func remove(_ key: KeyType) -> AnyPublisher<Void, Error>
 
   /// Asks to clear the cache level
